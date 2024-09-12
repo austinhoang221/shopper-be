e47abba447f417019ac93f3231d2dd4c4b9cf894
BinhPNT <binhpnt@quangich.com>
2024-09-12 10:42:15 +0700

    first-commit

---
+++ tests/Paris.RMS.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Paris.RMS.FunctionalTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Paris.RMS.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Paris.RMS.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
