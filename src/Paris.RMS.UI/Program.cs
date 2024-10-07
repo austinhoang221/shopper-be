68da816027c559df70ddcb79b587b9249dbd43fa
linhdt2311 <linhdepzais2311@gmail.com>
2024-10-07 22:15:55 +0700

    [SHOPPER-1] create project UI

---
+++ src/Paris.RMS.UI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+var withApiVersioning = builder.Services.AddDefaultVersioning();
+
+builder.AddDefaultAuthentication();
+
+builder.AddDefaultSwashbuckle(withApiVersioning);
+
+builder.Services
+    .RegisterServices()
+    .RegisterValidator()
+    .RegisterUseCasesLayer()
+    .RegisterPersistenceLayer(builder.Environment)
+    .AddCors()
+    ;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseDefaultSwashbuckle();
+
+app.UseDefaultAuthentication();
+
+app.MapControllers();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:3000"));
+
+app.Run();
