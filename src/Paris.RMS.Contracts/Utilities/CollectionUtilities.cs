93ad80ea2e8bb153fc47ebba26752125d07aa6fe
BinhPNT <binhpnt@quangich.com>
2024-10-08 15:57:07 +0700

    generate forest utilities

--- src/Paris.RMS.Contracts/Utilities/CollectionUtilities.cs
+++ src/Paris.RMS.Contracts/Utilities/CollectionUtilities.cs
@@ -11,4 +11,42 @@ public static class CollectionUtilities
 
         return true;
     }
+
+    /// <summary>
+    /// Generate a forest based on the input list
+    /// </summary>
+    /// <typeparam name="TOutput">Kiểu dữ liệu cho danh sách đầu ra</typeparam>
+    /// <typeparam name="TInput">Kiểu dữ liệu cho danh sách đầu vào</typeparam>
+    /// <typeparam name="TOrderKey">Kiểu dữ liệu dành cho order</typeparam>
+    /// <typeparam name="TMapping">Kiểu giữ liệu xác định mapping</typeparam>
+    /// <param name="collection">Danh sách dữ liệu đầu vào</param>
+    /// <param name="parentSelector">Lựa chọn cha</param>
+    /// <param name="orderSelector">Hàm sắp sếp các item</param>
+    /// <param name="createInstance">Hàm khởi tạo cho 1 item dữ liệu đầu ra</param>
+    /// <param name="mappingSelector">Hàm mapping cha con</param>
+    /// <param name="rootValue">Giá của cha gốc</param>
+    /// <returns>IEnumerable RT</returns>
+    public static IEnumerable<TOutput> GenerateForest<TOutput, TInput, TOrderKey, TMapping>(this IEnumerable<TInput> collection,
+        Func<TInput, TMapping> parentSelector,
+        Func<TInput, TOrderKey> orderSelector,
+        Func<TInput, TOutput> createInstance,
+        Func<TInput, TMapping> mappingSelector,
+        TMapping? rootValue = default)
+        where TOutput : class, INode<TOutput> where TInput : class
+    {
+        foreach (var c in collection.Where(c => rootValue?.Equals(parentSelector(c)) ?? false).OrderBy(q => orderSelector(q)))
+        {
+            var instance = createInstance(c);
+            instance.Children = collection.GenerateForest(parentSelector, orderSelector, createInstance, mappingSelector, mappingSelector(c));
+            yield return instance;
+        }
+    }
+}
+
+public interface INode<T>
+{
+    /// <summary>
+    /// danh sách các con của đối tượng
+    /// </summary>
+    public IEnumerable<T> Children { get; set; }
 }
---
+++ tests/Paris.RMS.UnitTests/CollectionUtilitiesTests.cs
@@ -0,0 +1,47 @@
+﻿using Paris.RMS.Contracts.Utilities;
+
+namespace Paris.RMS.UnitTests;
+
+public class CollectionUtilitiesTests
+{
+    [Fact]
+    public void GenerateTreeTest()
+    {
+        List<InputTreeClass> datas =
+            [
+                new("01", "Item 01", null, 1),
+                new("02", "Item 02", null, 0),
+                new("03", "Item 03",  "01", 0),
+                new("04", "Item 04",  "01", 1),
+                new("05", "Item 05",  "02", 1),
+                new("06", "Item 06",  "02", 0),
+            ];
+
+        var forest = datas
+            .GenerateForest(q => q.ParentId ?? "",
+                q => q.Order,
+                q => new OutputTreeClass(q.Id, q.Name, q.ParentId),
+                q => q.Id,
+                "")
+            .ToList();
+
+        Assert.Equal(2, forest.Count);
+    }
+}
+
+public class InputTreeClass(string id, string name, string? parentId, int order)
+{
+    public string Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public string? ParentId { get; set; } = parentId;
+    public int Order { get; set; } = order;
+}
+
+public class OutputTreeClass(string value, string label, string? parentValue)
+    : INode<OutputTreeClass>
+{
+    public string Value { get; set; } = value;
+    public string Label { get; set; } = label;
+    public string? ParentValue { get; set; } = parentValue;
+    public IEnumerable<OutputTreeClass> Children { get; set; } = [];
+}
